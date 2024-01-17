e05b0ec82ea910095c8d6a81646965d5d8bdb914
yujen77300 <yujenhuang24@gmail.com>
2024-01-17 23:17:17 +0800

    add: L6 - pointers

---
+++ pointer/wallet_test.go
@@ -0,0 +1,51 @@
+package pointer
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want string) {
+		if got == nil {
+			// fatal如果被觸發 會直接跳出，不會繼續往下執行
+			t.Fatal("didn't get an error but wanted one")
+		}
+	
+		if got.Error() != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+	
+		// assertBalance(t, wallet, startingBalance)
+	
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, "cannot withdraw, insufficient funds")
+	})
+
+}
