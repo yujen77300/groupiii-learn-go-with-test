56580404f87cc53d87ea8f652947da6f6d67eac0
yujen77300 <yujenhuang24@gmail.com>
2024-02-22 07:45:00 +0800

    update : mock

--- mock/mock.go
+++ mock/mock.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{duration: 1 * time.Second}
+	Countdown(os.Stdout, sleeper)
+}
--- mock/notify/notifymock.go
+++ mock/notify/notifymock.go
@@ -1,5 +1,10 @@
 package mock
 
+// 這個系統將允許不同的消息發送方式（如電子郵件、短信或推送通知）來實現同一接口
+// MessageService 依賴於一個 Notifier 接口，這個接口的實現決定了消息的具體發送方式。
+// 創建一個MessageService 實例時，你會將一個具體的 Notifier（例如 EmailNotifier、SmsNotifier 或 PushNotifier）注入到 MessageService。
+// 這樣，MessageService 就可以使用這個注入的 Notifier 來發送消息，而不用關心消息是如何發送的。
+
 type Notifier interface {
 	Send(message string) error
 }
@@ -17,3 +22,4 @@ func NewMessageService(notifier Notifier) *MessageService {
 func (m *MessageService) SendAlert(message string) error {
 	return m.notifier.Send(message)
 }
+
